e1230002ebdeb1746f3b16293e9a03420ed8c70c
Marx Medeiros <analista.marx@gmail.com>
2016-10-03 11:57:36 -0300

    Adicionado Idioma

--- Produto/Categoria.php
+++ Produto/Categoria.php
@@ -30,6 +30,7 @@ $Catt->execute();
 <html>
 <head>
   <meta charset="utf-8">
+  <meta http-equiv="Content-Language" content="pt-br">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php echo $Titulo; ?></title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
