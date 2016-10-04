ca8c8bccabd191940fc444c601552af3266d2631
Marx Medeiros <analista.marx@gmail.com>
2016-10-04 16:33:53 -0300

    Adaptação

--- dashboard.php
+++ dashboard.php
@@ -1,13 +1,25 @@
 <?php
-require("restritos.php"); 
-require_once 'init.php';
+//MENU LATERAL ATIVO
+$cNotas = "active";
+
+
+require("../restritos.php"); 
+require_once '../init.php';
 $PDO = db_connect();
  $query = $PDO->prepare("SELECT * FROM login WHERE login='$login'");
  $query->execute();
   $row = $query->fetch();
   $NomeUserLogado = $row['Nome'];
   $foto = $row['Foto'];
-  $cHome = "active";
+
+
+ $privilegio = $PDO->prepare("SELECT * FROM privilegio WHERE idUser='$login'");
+ $privilegio->execute();
+  $cp = $privilegio->fetch();
+  $VerProdutos = $cp['pProduto'];
+  $VProd = $cp['vIP'];            //VER ITENS DE PRODUÇÃO
+  $CadProd = $cp['cIProd'];       //CADASTRAR ITENS DE PRODUÇÃO
+  $VArPro = $cp['lisArPro'];      //LISTAR ARVORE DE PRODUTO
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,27 +29,27 @@ $PDO = db_connect();
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php echo $Titulo; ?></title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
-  <link rel="stylesheet" href="plugins/iCheck/flat/blue.css">
-  <link rel="stylesheet" href="plugins/morris/morris.css">
-  <link rel="stylesheet" href="plugins/jvectormap/jquery-jvectormap-1.2.2.css">
-  <link rel="stylesheet" href="plugins/datepicker/datepicker3.css">
-  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
-  <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="../plugins/iCheck/flat/blue.css">
+  <link rel="stylesheet" href="../plugins/morris/morris.css">
+  <link rel="stylesheet" href="../plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+  <link rel="stylesheet" href="../plugins/datepicker/datepicker3.css">
+  <link rel="stylesheet" href="../plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="../plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
 </head>
 <body class="hold-transition <?php echo $cor; ?> fixed sidebar-mini">
  <div class="wrapper">
   <header class="main-header">
    <a href="#" class="logo">
     <span class="logo-mini">
-     <img src="dist/img/logo/logoWhite.png" width="50"/>
+     <img src="../dist/img/logo/logoWhite.png" width="50"/>
     </span>
     <span class="logo-lg">
-     <img src="dist/img/logo/logoWhite.png" width="180" />
+     <img src="../dist/img/logo/logoWhite.png" width="180" />
     </span>
    </a>
    <nav class="navbar navbar-static-top">
@@ -48,12 +60,12 @@ $PDO = db_connect();
      <ul class="nav navbar-nav">
       <li class="dropdown user user-menu">
        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-        <img src="dist/img/user/<?php echo $foto; ?>" class="user-image">
+        <img src="../dist/img/user/<?php echo $foto; ?>" class="user-image">
         <span class="hidden-xs"><?php echo $NomeUserLogado; ?></span>
        </a>
        <ul class="dropdown-menu">
         <li class="user-header">
-         <img src="dist/img/user/<?php echo $foto; ?>" class="img-circle">
+         <img src="../dist/img/user/<?php echo $foto; ?>" class="img-circle">
          <p><?php echo $NomeUserLogado; ?></p>
         </li>
         <li class="user-footer">
@@ -77,113 +89,59 @@ $PDO = db_connect();
    <section class="sidebar">
     <div class="user-panel">
      <div class="pull-left image">
-      <img src="dist/img/user/<?php echo $foto; ?>" class="img-circle">
+      <img src="../dist/img/user/<?php echo $foto; ?>" class="img-circle">
      </div>
      <div class="pull-left info">
       <p><?php echo $NomeUserLogado; ?></p>
      </div>
     </div>
-    <?php include_once 'menuLateral.php'; ?>
+    <?php include_once '../menuLateral.php'; ?>
     </section>
   </aside>
   <div class="content-wrapper">
    <section class="content-header">
-    <h1>Página Inicial<small><?php echo $Titulo; ?></small></h1>
+    <h1>Controle de Estoque<small><?php echo $Titulo; ?></small></h1>
    </section>
    <section class="content">
    <div class="row">
-    <div class="col-md-4">
-     <div class="info-box">
-      <a href="fornecedor/dashboard.php" >
-       <span class="info-box-icon btn-danger">
-        <i class="fa fa-briefcase"></i></span>
-      </a>
-      <div class="info-box-content"><h4>Fornecedor</h4></div>
-     </div>
-     </div>
-     <div class="col-md-4"> 
-      <div class="info-box">
-       <a href="Transporte.php" >
-        <span class="info-box-icon btn-info">
-         <i class="fa fa-truck"></i>
-        </span>
-       </a>
-       <div class="info-box-content"><h4>Transportadoras</h4></div>
-      </div>
-     </div>
-     <div class="col-md-4">
-      <div class="info-box">
-       <a href="Produto/dashboard.php" >
-        <span class="info-box-icon btn-black">
-         <i class="fa fa-sort-alpha-asc"></i>
-        </span>
-       </a>
-       <div class="info-box-content"><h4>Tabela de itens</h4></div>
-      </div>
-     </div>
-     <div class="col-md-4">
-      <div class="info-box">
-       <a href="suporte/dashboard.php" >
-        <span class="info-box-icon bg-orange">
-         <i class="fa fa-list"></i>
-        </span>
-       </a>
-       <div class="info-box-content"><h4>Controle de Firmware</h4></div>
-      </div>
-     </div>
-     <div class="col-md-4">
-      <div class="info-box">
-       <a href="fw/dashboard.php" >
-        <span class="info-box-icon bg-orange">
-         <i class="fa fa-list"></i>
-        </span>
-       </a>
-       <div class="info-box-content"><h4>Suporte Técnico</h4></div>
-      </div>
-     </div>
-   </div>
-   </section>
   </div>
-  <?php include_once 'footer.php'; ?>
+ </section>
 </div>
-<!-- ./wrapper -->
-
-<!-- jQuery 2.2.3 -->
-<script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button);
-</script>
-<!-- Bootstrap 3.3.6 -->
-<script src="bootstrap/js/bootstrap.min.js"></script>
-<!-- Morris.js charts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="plugins/morris/morris.min.js"></script>
-<!-- Sparkline -->
-<script src="plugins/sparkline/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
-<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="plugins/knob/jquery.knob.js"></script>
-<!-- daterangepicker -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-<script src="plugins/daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
-<script src="plugins/datepicker/bootstrap-datepicker.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
-<script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="plugins/fastclick/fastclick.js"></script>
+  <?php include_once '../footer.php'; ?>
+</div>
+<script src="../plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+<script src="../plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../plugins/datatables/dataTables.bootstrap.min.js"></script>
+<script src="../plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<script src="../plugins/fastclick/fastclick.js"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/app.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard.js"></script>
+<script src="../dist/js/app.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
+<script src="../dist/js/demo.js"></script>
+<!-- page script -->
+<script>
+  $(function () {
+    $("#example1").DataTable();
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false
+    });
+  });
+</script>
+<script language="JavaScript">
+function abrir(URL) { 
+  var width = 1100;
+  var height = 650;
+  var left = 99;
+  var top = 99;
+  window.open(URL,'janela', 'width='+width+', height='+height+', top='+top+', left='+left+', scrollbars=yes, status=no, toolbar=no, location=no, directories=no, menubar=no, resizable=no, fullscreen=no');
+ 
+}
+</script>
 </body>
 </html>
